88c506c0a3b527e673a956803895e67786d24eda
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-06-14 21:53:15 +0300

    Create endpoints

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./db');
 const models = require('./db/models/models');
+const cors = require('cors');
+const router = require('./routes/index');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
