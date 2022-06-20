538f005c0bcb82506e534fe662b3534c9108e138
Aleh Barushka <79603153+AlehBarushka@users.noreply.github.com>
2022-06-20 20:18:52 +0300

    Add check user role middleware (#7)
    * Some bugfix
    
    * Added check role middleware

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const deviceController = require('../controllers/deviceController');
+const checkRole = require('../middleware/chekRoleMiddleware');
 
 const router = new Router();
 
-router.post('/', deviceController.create);
+router.post('/', checkRole('ADMIN'), deviceController.create);
 router.get('/', deviceController.get);
 router.get('/:id', deviceController.getOne);
 
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const typeController = require('../controllers/typeController');
+const checkRole = require('../middleware/chekRoleMiddleware');
 
 const router = new Router();
 
-router.post('/', typeController.create);
+router.post('/', checkRole('ADMIN'), typeController.create);
 router.get('/', typeController.get);
 
 module.exports = router;
